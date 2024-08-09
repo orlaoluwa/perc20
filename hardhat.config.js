82cec2fd61bf6b45c789e5d646f99440f9a9eefd
Orlaoluwa <infodjyoungstarlheey@gmail.com>
2024-08-10 00:20:01 +0100

    push

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["0x730c4cd1945af21ea12a8eb2cc1232c547e34f33de6633436f52b2ea88bd5c57"],
+      accounts: ["0x"],
     },
   },
 };
